dab822975ba362686b84158befee9692b92891a3
Orion Osborn <orion@Orions-MBP-3.attlocal.net>
2023-07-22 13:05:48 -0400

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Farm to Kitchen</h1>
+        <h3>Are you a</h3>
+        <span>
+          <Button variant="contained">Farmer</Button>
+          <span style={{marginRight: 10, marginLeft: 10, fontSize: 20}}>or</span>
+          <Button variant="contained">Customer</Button>
+        </span>
       </header>
     </div>
   );
