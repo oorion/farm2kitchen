29328fd25c0206da9530654f2bd620cf761d29bd
Orion Osborn <orion.osborn@gmail.com>
2023-07-24 11:49:29 -0400

    add ability to update items in store on what to sell page

--- src/routes/WhatToSell.js
+++ src/routes/WhatToSell.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { useDispatch } from 'react-redux'
+import { addItem } from '../reducers'
 
 function WhatToSell() {
-    const startFarmerIntake = () => {
-      
-    }
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Farm to Kitchen</h1>
-          <h3>What do you have to sell?</h3>
-          <span>
-            <Form inline style={{display: 'flex'}}>
-              <Form.Control type="textarea" placeholder="Description" />
-              <Form.Control type="text" placeholder="Quantity" />
-              <Button variant="success">Enter</Button>
-            </Form>
-          </span>
-        </header>
-      </div>
-    );
+  const [description, setDescription] = useState('')
+  const [quantity, setQuantity] = useState('')
+  const dispatch = useDispatch()
+  const add = () => {
+    dispatch(addItem({description: description, quantity: quantity}))
   }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Farm to Kitchen</h1>
+        <h3>What do you have to sell?</h3>
+        <span>
+          <Form inline='true' style={{display: 'flex'}}>
+            <Form.Control type="textarea" placeholder="Description" onChange={event => setDescription(event.target.value)} />
+            <Form.Control type="text" placeholder="Quantity" onChange={event => setQuantity(event.target.value)} />
+            <Button variant="success" onClick={add}>Enter</Button>
+          </Form>
+        </span>
+      </header>
+    </div>
+  );
+}
   
   export default WhatToSell;
